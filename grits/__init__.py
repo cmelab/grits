30c6f2f9748a79bdfc529bd6eeb4ab6fcb1fe3f0
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-22 18:53:20 -0600

    add backmap

--- grits/__init__.py
+++ grits/__init__.py
@@ -1,5 +1,6 @@
 from .__version__ import __version__
 from .cg_compound import CG_Compound
+from .backmap import backmap
 from . import utils
 
-__all__ = ["__version__", "CG_Compound", "utils"]
+__all__ = ["__version__", "CG_Compound", "utils", "backmap"]
