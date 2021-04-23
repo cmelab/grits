5849a8fa654d21af491d3bfa9e63d970ecf95d8d
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-23 11:07:45 -0600

    add module level docstrings

--- grits/__init__.py
+++ grits/__init__.py
@@ -1,6 +1,7 @@
-from .__version__ import __version__
-from .cg_compound import CG_Compound
-from .backmap import backmap
+"""GRiTS: A coarse-grain toolkit using chemical grammars."""
 from . import utils
+from .__version__ import __version__
+from .backmap import backmap
+from .cg_compound import CG_Compound
 
 __all__ = ["__version__", "CG_Compound", "utils", "backmap"]
