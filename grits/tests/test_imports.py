478b86287a31d50bf03f43002bcc42ace3ebd763
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-22 19:08:25 -0600

    add backmap

--- grits/tests/test_imports.py
+++ grits/tests/test_imports.py
@@ -2,4 +2,6 @@ def test_imports():
     import grits
     import grits.cg_compound
     import grits.utils
+    import grits.backmap
     from grits import CG_Compound
+    from grits import backmap
