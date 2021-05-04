9f2246e8c850496c079a9c42ff70d04ca398618c
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-05-04 17:30:21 -0600

    simplified alignment.
    - two align steps are still in the comments. will remove.

--- grits/finegrain.py
+++ grits/finegrain.py
@@ -42,7 +42,9 @@ def backmap(cg_compound):
         """Set the bonds for the fine-grained structure."""
         bonded_atoms = []
         remove_hs = []
-        rotated = {k: [False, False] for k in anchors.keys()}
+        # The commented out bits are for two align steps
+        # rotated = {k: [False, False] for k in anchors.keys()}
+        rotated = {k: False for k in anchors.keys()}
         for name, inds in cg_compound.bond_map:
             for ibead, jbead in cg_compound.bonds():
                 names = [ibead.name, jbead.name]
@@ -70,16 +72,20 @@ def backmap(cg_compound):
                 hj = get_hydrogen(fine_grained, jatom)
                 # each part can be rotated twice
                 # first arbitrarily and second along the first rotation axis
-                if rotated[i][0] is False:
+                # if rotated[i][0] is False:
+                if not rotated[i]:
                     # rotate
-                    rotated[i][0] = align(fine_grained[str(i)], hi, jbead)
+                    align(fine_grained[str(i)], hi, jbead)
+                    rotated[i] = True
                 # elif not rotated[i][1]:
                 #    print(i, " rotate twice")
                 #    align(fine_grained[str(i)], hi, jbead, around=rotated[i][0])
                 #    rotated[i][1] = True
-                if rotated[j][0] is False:
+                if not rotated[j]:
                     # rotate
-                    rotated[j][0] = align(fine_grained[str(j)], hj, ibead)
+                    # rotated[j][0] = align(fine_grained[str(j)], hj, ibead)
+                    align(fine_grained[str(j)], hj, ibead)
+                    rotated[j] = True
                 # elif not rotated[j][1]:
                 #    print(j, " rotate once")
                 #    align(fine_grained[str(j)], hj, ibead, around=rotated[j][0])
