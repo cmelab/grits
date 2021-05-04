b2fa172fe6dccd588cfa08a737e23e7ab171abb8
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-05-04 17:22:15 -0600

    - updated get_hydrogens to be deterministic,
    - added note about issue in get_bonds, but sorting is too slow
    - added align function

--- grits/utils.py
+++ grits/utils.py
@@ -4,6 +4,48 @@ import re
 import numpy as np
 
 
+def align(compound, particle, towards_compound, around=None):
+    """Spin a compound such that particle points at towards_compound.
+
+    Parameters
+    ----------
+    compound: mbuild.Compound or CG_Compound,
+        The compound to align
+    particle: mbuild.Compound or CG_Compound,
+        The particle to point at towards_compound. Child of compound.
+    towards_compound: mbuild.Compound or CG_Compound,
+        The compound to point towards.
+    around: numpy.array, default None
+        The unit vector around which the compound is spun. If None is given,
+        an orthogonal vector is chosen.
+
+    Returns
+    -------
+    numpy.array
+        The unit vector about which the compound is rotated
+    """
+    # Find the unit vector from compound center to particle
+    vec = np.array(particle.pos - compound.center)
+    sep = np.linalg.norm(vec)
+    comp_to_part = vec / sep
+
+    # Get the unit vector between the two centers
+    # end - start: from 1 -> 2
+    vec = np.array(towards_compound.center - compound.center)
+    sep = np.linalg.norm(vec)
+    towards_to_comp = vec / sep
+
+    if around is None:
+        # Next get a vector orthogonal to both vectors,
+        # this is the vector around which the compound is spun
+        around = np.cross(comp_to_part, towards_to_comp)
+    # and the angle between the two vectors (in rad)
+    angle = np.arccos(np.dot(comp_to_part, towards_to_comp))
+
+    compound.spin(angle, around)
+    return around
+
+
 def get_bonds(compound):
     """Convert Particle instances in bond_graph.bond_edges to their indices.
 
@@ -48,7 +90,12 @@ def get_bonded(compound, particle):
         else:
             return False
 
-    return [is_particle(i, j) for i, j in compound.bonds() if is_particle(i, j)]
+    xs = [is_particle(i, j) for i, j in compound.bonds() if is_particle(i, j)]
+    # The following logic won't be necessary when bond graph is deterministic
+    # https://github.com/mosdef-hub/mbuild/issues/895
+    # and instead we can just do:
+    return xs
+    # return [x for _,x in sorted(zip([get_index(compound,i) for i in xs],xs))]
 
 
 def get_index(compound, particle):
@@ -71,7 +118,7 @@ def get_index(compound, particle):
 
 
 def remove_hydrogen(compound, particle):
-    """Remove one hydrogen attached to particle.
+    """Remove the first hydrogen attached to particle.
 
     Hydrogen particle name must be "H". If no hydrogen is bonded to particle,
     this function will do nothing.
@@ -83,9 +130,28 @@ def remove_hydrogen(compound, particle):
     particle: mbuild.Particle or Bead
         Particle from which to remove a hydrogen
     """
+    hydrogen = get_hydrogen(compound, particle)
+    if hydrogen is not None:
+        compound.remove(hydrogen)
+
+
+def get_hydrogen(compound, particle):
+    """Get the first hydrogen attached to particle.
+
+    Parameters
+    ----------
+    compound: mbuild.Compound or CG_Compound
+        Compound which contains particle
+    particle: mbuild.Particle or Bead
+        Particle from which to remove a hydrogen
+    """
     hydrogens = [i for i in get_bonded(compound, particle) if i.name == "H"]
     if hydrogens:
-        compound.remove(hydrogens[0])
+        for p in compound:
+            if p in hydrogens:
+                return p
+    else:
+        return None
 
 
 def has_number(string):
