57aa673b5a10246be02ee8e063d978a47ceb2587
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-05-03 18:01:53 -0600

    update the backmap to use CG_Compound attributes

--- grits/finegrain.py
+++ grits/finegrain.py
@@ -6,7 +6,7 @@ from mbuild import Compound, Particle, load
 from grits.utils import distance, get_index, remove_hydrogen
 
 
-def backmap(cg_compound, bead_dict, bond_dict=None):
+def backmap(cg_compound):
     """Backmap a fine-grained representation onto a coarse one.
 
     Creates a fine-grained compound from a coarse one given dictionaries
@@ -16,33 +16,6 @@ def backmap(cg_compound, bead_dict, bond_dict=None):
     ----------
     cg_compound: CG_Compound
         coarse-grained compound
-    bead_dict: dictionary of dictionaries
-        specifies what SMILES string and bond anchors to use for each bead type.
-        For example:
-
-        >>> bead_dict = {
-        >>>     "_B": {
-        >>>         "smiles": "c1sccc1",
-        >>>         "anchors": [0,2,4],
-        >>>         "posres": 1
-        >>>         },
-        >>>     }
-
-        specifies that coarse grain bead `"_B"` should be replaced with the
-        fine-grain structure represented by the SMILES string `"c1sccc1"`,
-        should form bonds to other fine-grained beads from atoms `[0, 2, 4]`,
-        and should have a position restraint attached to atom `1`.
-        The position restraint and anchors are optional.
-    bond_dict: dictionary of list of tuples
-        specifies what fine-grain bond should replace the bond in the coarse
-        structure. For example:
-
-        >>>     bond_dict = {
-        >>>         "_B_B": [(0,2),(2,0)],
-        >>>         }
-
-        specifies that the bond between two `"_B"` beads should be replaced in
-        the fine-grain structure between the atoms `(0, 2)` or `(2, 0)`.
 
     Returns
     -------
@@ -50,65 +23,46 @@ def backmap(cg_compound, bead_dict, bond_dict=None):
     """
 
     def fg_particles():
-        """Set the particles ofr the fine-grained structure."""
+        """Set the particles of the fine-grained structure."""
         fine_grained = Compound()
 
         anchors = dict()
         for i, bead in enumerate(cg_compound):
-            smiles = bead_dict[bead.name]["smiles"]
+            smiles = bead.smarts
             b = load(smiles, smiles=True)
             b.translate_to(bead.pos)
             anchors[i] = dict()
-            for index in bead_dict[bead.name]["anchors"]:
+            for index in cg_compound.anchors[bead.name]:
                 anchors[i][index] = b[index]
-            try:
-                posres_ind = bead_dict[bead.name]["posres"]
-                posres = Particle(name="X", pos=bead.pos)
-                b.add(posres)
-                b.add_bond((posres, b[posres_ind]))
-            except KeyError:
-                pass
             fine_grained.add(b)
         return fine_grained, anchors
 
     def fg_bonds():
         """Set the bonds for the fine-grained structure."""
         bonded_atoms = []
-        for ibead, jbead in cg_compound.bonds():
-            i = get_index(cg_compound, ibead)
-            j = get_index(cg_compound, jbead)
-            names = [ibead.name, jbead.name]
-            bondname = "".join(names)
-            try:
-                bonds = bond_dict[bondname]
-            except KeyError:
-                try:
-                    bondname = "".join(names[::-1])
-                    bonds = [(j, i) for (i, j) in bond_dict[bondname]]
-                except KeyError:
-                    raise KeyError(
-                        f"{bondname} not defined in bond dictionary."
-                    )
-            # choose a starting distance that is way too big
-            mindist = max(cg_compound.boundingbox.lengths)
-            for fi, fj in bonds:
-                iatom = anchors[i][fi]
-                jatom = anchors[j][fj]
-                if (iatom in bonded_atoms) or (jatom in bonded_atoms):
-                    # assume only one bond from the CG translates
-                    # to the FG structure
+        for name, inds in cg_compound.bond_map:
+            for ibead, jbead in cg_compound.bonds():
+                names = [ibead.name, jbead.name]
+                if "-".join(names) == name:
+                    pass
+                elif "-".join(names[::-1]) == name:
+                    inds = inds[::-1]
+                    print(inds)
+                else:
                     continue
-                dist = distance(iatom.pos, jatom.pos)
-                if dist < mindist:
-                    fi_best = fi
-                    fj_best = fj
-                    mindist = dist
-            iatom = anchors[i][fi_best]
-            jatom = anchors[j][fj_best]
-            fine_grained.add_bond((iatom, jatom))
 
-            bonded_atoms.append(iatom)
-            bonded_atoms.append(jatom)
+                i = get_index(cg_compound, ibead)
+                j = get_index(cg_compound, jbead)
+                print(name, inds)
+                iatom = anchors[i][inds[0]]
+                jatom = anchors[j][inds[1]]
+
+                if any(x in bonded_atoms for x in [iatom, jatom]):
+                    continue
+                fine_grained.add_bond((iatom, jatom))
+
+                bonded_atoms.append(iatom)
+                bonded_atoms.append(jatom)
 
         for atom in bonded_atoms:
             remove_hydrogen(fine_grained, atom)
@@ -116,7 +70,7 @@ def backmap(cg_compound, bead_dict, bond_dict=None):
 
     fine_grained, anchors = fg_particles()
 
-    if bond_dict is None:
+    if cg_compound.bond_map is None:
         return fine_grained
 
     fine_grained = fg_bonds()
