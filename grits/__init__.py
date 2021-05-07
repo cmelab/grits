4f21baa2d1e8557ccc670f3d2b9b608e0e716f2b
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-05-06 18:59:05 -0600

    trying a new fix

--- grits/__init__.py
+++ grits/__init__.py
@@ -1,7 +1,7 @@
 """GRiTS: A coarse-grain toolkit using chemical grammars."""
-from . import utils
-from .__version__ import __version__
-from .coarsegrain import CG_Compound
-from .finegrain import backmap
-
-__all__ = ["__version__", "CG_Compound", "utils", "backmap"]
+# from . import utils
+# from .__version__ import __version__
+# from .coarsegrain import CG_Compound
+# from .finegrain import backmap
+#
+# __all__ = ["__version__",]# "CG_Compound", "utils", "backmap"]
