9bc889e471a02a505cd7d7888dcc31087b97ccd7
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-05-11 17:52:59 -0600

    add tests for init from mapping

--- grits/tests/test_coarsegrain.py
+++ grits/tests/test_coarsegrain.py
@@ -5,7 +5,7 @@ from grits import CG_Compound
 
 
 class Test_CGCompound(BaseTest):
-    def test_init_methane(self, methane):
+    def test_initmethane(self, methane):
         cg_beads = {"_A": "C"}
 
         cg_methane = CG_Compound(methane, cg_beads)
@@ -17,7 +17,7 @@ class Test_CGCompound(BaseTest):
         assert "_A" in types
         assert len(types) == 1
 
-    def test_init_p3ht(self, p3ht):
+    def test_initp3ht(self, p3ht):
         cg_beads = {"_B": "c1sccc1", "_S": "CCC"}
 
         cg_p3ht = CG_Compound(p3ht, cg_beads)
@@ -29,6 +29,27 @@ class Test_CGCompound(BaseTest):
         assert "_B" in types
         assert "_S" in types
         assert len(types) == 2
+
+    def test_initmapp3ht(self, p3ht, p3ht_mapping):
+        cg_p3ht = CG_Compound(p3ht, mapping=p3ht_mapping)
+
+        assert cg_p3ht.n_particles == 48
+        assert isinstance(cg_p3ht, CG_Compound)
+
+        types = set([i.name for i in cg_p3ht.particles()])
+        assert "_B" in types
+        assert "_S" in types
+        assert len(types) == 2
+
+    def test_initmapmethane(self, methane, methane_mapping):
+        cg_methane = CG_Compound(methane, mapping=methane_mapping)
+
+        assert cg_methane.n_particles == 1
+        assert isinstance(cg_methane, CG_Compound)
+
+        types = set([i.name for i in cg_methane.particles()])
+        assert "_A" in types
+        assert len(types) == 1
 
     def test_notfoundsmarts(self, methane):
         cg_beads = {"_A": "CCC"}
