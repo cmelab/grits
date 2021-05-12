682d18e88ea69a8c7427c13f51686356dd512473
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-05-12 16:19:09 -0600

    remove openbabel and rdkit from setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ AUTHOR = "Jenny Fothergill"
 REQUIRES_PYTHON = ">=3.7.0"
 
 # What packages are required for this module to be executed?
-REQUIRED = ["mbuild", "numpy", "openbabel", "rdkit"]
+REQUIRED = ["mbuild", "numpy"]
 
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
