6af011048b0c893f55f2f86e3bccff00d3eee076
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2024-01-21 20:38:27 -0700

    finish up fg func, fix some bugs

--- grits/__init__.py
+++ grits/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 from . import utils
 from .coarsegrain import Bead, CG_Compound, CG_System
-from .finegrain import backmap
+#from .finegrain import backmap
 
 try:
     __version__ = version("grits")
--- grits/finegrain.py
+++ grits/finegrain.py
@@ -1,15 +1,88 @@
 """GRiTS: Fine-graining tools."""
-__all__ = ["backmap"]
+__all__ = ["backmap_compound", "backmap_gsd"]
 
 import itertools as it
 from collections import defaultdict
 
 from mbuild import Compound, Particle, load
+import mbuild as mb
+import numpy as np
+import gsd.hoomd
 
 from grits.utils import align, get_hydrogen, get_index
 
+def backmap_snapshot_to_compound(
+        snapshot,
+        bead_mapping,
+        bond_head_index=dict(),
+        bond_tail_index=dict()
+):
+    #TODO
+    # assert all 3 dicts have the same keys
+    cg_snap = snapshot
+    fg_snap = gsd.hoomd.Frame()
+    fg_compound = mb.Compound()
+    box = cg_snap.configuration.box
+    mb_box = mb.box.Box.from_lengths_angles(
+            lengths=[box[0], box[1], box[2]],
+            angles=[np.pi, np.pi, np.pi]
+    )
+    fg_compound.box = mb_box
+    # Create atomistic compounds, remove hydrogens in the way
+    compounds = dict() 
+    anchor_dict = dict()
+    for mapping in bead_mapping:
+        comp = mb.load(bead_mapping[mapping], smiles=True)
+        if bond_head_index and bond_tail_index:
+            remove_hydrogens = [] # These will be removed
+            anchor_particles = [] # Store this for making bonds later
+            for i in [bond_tail_index[mapping], bond_head_index[mapping]]:
+                for j, particle in enumerate(comp.particles()):
+                    if j == i:
+                        remove_hydrogens.append(particle)
+                        anchor = [p for p in particle.direct_bonds()][0]
+                        anchor_particles.append(anchor)
+            for particle in remove_hydrogens:
+                comp.remove(particle)
+            # List of length 2 [tail particle index, head particle index]
+            anchor_particle_indices = []
+            for anchor in anchor_particles:
+                for i, p in enumerate(comp.particles()):
+                    if p == anchor:
+                        anchor_particle_indices.append(i)
+            anchor_dict[mapping] = tuple(anchor_particle_indices)
 
-def backmap(cg_compound):
+        compounds[mapping] = comp
+
+    finished_beads = set()
+    bead_to_comp_dict = dict()
+    mb_compounds = []
+    for group in cg_snap.bonds.group:
+        for bead_index in group:
+            if bead_index not in finished_beads:
+                # add compoud to snapshot
+                bead_type = cg_snap.particles.types[
+                        cg_snap.particles.typeid[bead_index]
+                ]
+                bead_pos = cg_snap.particles.position[bead_index]
+                comp = mb.clone(compounds[bead_type])
+                comp.translate_to(bead_pos)
+                mb_compounds.append(comp)
+                bead_to_comp_dict[bead_index] = comp
+                finished_beads.add(bead_index)
+                fg_compound.add(comp)
+
+        tail_comp = bead_to_comp_dict[group[0]]
+        tail_comp_particle = tail_comp[anchor_particle_indices[1]]
+        head_comp = bead_to_comp_dict[group[1]]
+        head_comp_particle = head_comp[anchor_particle_indices[0]]
+        fg_compound.add_bond(particle_pair=[tail_comp_particle, head_comp_particle])
+    return  fg_compound
+
+
+
+
+def backmap_compound(cg_compound):
     """Backmap a fine-grained representation onto a coarse one.
 
     Creates a fine-grained compound from a coarse one using the attributes
