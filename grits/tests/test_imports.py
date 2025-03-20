9676c7bb0d81f2216dcb08d050ff24b968076d73
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2025-03-20 14:54:01 -0600

    add tests back to test_imports

--- grits/tests/test_imports.py
+++ grits/tests/test_imports.py
@@ -1,2 +1,7 @@
+# ruff: noqa: F401
 def test_imports():
-    pass
+    import grits
+    import grits.coarsegrain
+    import grits.finegrain
+    import grits.utils
+    from grits import CG_Compound, backmap
